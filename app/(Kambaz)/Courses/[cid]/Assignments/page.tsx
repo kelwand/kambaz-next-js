cdb7752924c3752c36c7eee4423df567c8d29730
kelseywandera <wandera.k@northeastern.edu>
2025-09-18 16:36:48 -0400

    Update AssignmentEditor and Kambaz pages for Next.js navigation

--- app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
@@ -1,29 +1,31 @@
+import Link from "next/link";
+
 export default function Assignments() {
   return (
     <div id="wd-assignments">
-      <input placeholder="Search for Assignments"
-             id="wd-search-assignment" />
+      <input placeholder="Search for Assignments" id="wd-search-assignment" />
       <button id="wd-add-assignment-group">+ Group </button>
       <button id="wd-add-assignment">+ Assignment </button>
       <h3 id="wd-assignments-title">
-        ASSIGNMENTS 40% of Total <button> + </button> </h3>
+        ASSIGNMENTS 40% of Total <button> + </button>
+      </h3>
       <ul id="wd-assignment-list">
         <li className="wd-assignment-list-item">
-          <a href="/Courses/1234/Assignments/123"
-             className="wd-assignment-link" >
+          <Link href="/Courses/1234/Assignments/123" className="wd-assignment-link">
             A1 - ENV + HTML
-          </a> 
+          </Link>
           <div className="wd-assignment-meta">
             <span>Multiple Modules</span>
             <span> | <strong> Not available until </strong> May 6 at 12:00am </span>
             <span> | <strong> Due </strong> May 13 at 11:59pm </span>
             <span> | 100 pts</span>
           </div>
-          </li>
+        </li>
+
         <li className="wd-assignment-list-item">
-            <a href="/Courses/1234/Assignments/124" className="wd-assignment-link">
+          <Link href="/Courses/1234/Assignments/124" className="wd-assignment-link">
             A2 - CSS + BOOTSTRAP
-          </a>
+          </Link>
           <div className="wd-assignment-meta">
             <span>Multiple Modules</span>
             <span> | <strong> Not available until </strong> May 13 at 12:00am </span>
@@ -33,9 +35,9 @@ export default function Assignments() {
         </li>
 
         <li className="wd-assignment-list-item">
-          <a href="/Courses/1234/Assignments/125" className="wd-assignment-link">
+          <Link href="/Courses/1234/Assignments/125" className="wd-assignment-link">
             A3 - JAVASCRIPT + REACT
-          </a>
+          </Link>
           <div className="wd-assignment-meta">
             <span>Multiple Modules</span>
             <span> | <strong> Not available until </strong> May 20 at 12:00am </span>
@@ -43,11 +45,11 @@ export default function Assignments() {
             <span> | 100 pts</span>
           </div>
         </li>
+
         <li className="wd-assignment-list-item">
-          {/* Complete On Your Own */}
-          <a href="/Courses/1234/Assignments/126" className="wd-assignment-link">
+          <Link href="/Courses/1234/Assignments/126" className="wd-assignment-link">
             A4 - STATE & REDUX
-          </a>
+          </Link>
           <div className="wd-assignment-meta">
             <span>Multiple Modules</span>
             <span> | <strong> Not available until </strong> May 27 at 12:00am </span>
@@ -57,9 +59,9 @@ export default function Assignments() {
         </li>
 
         <li className="wd-assignment-list-item">
-          <a href="/Courses/1234/Assignments/127" className="wd-assignment-link">
-           A5 - NODE AND SESSION
-          </a>
+          <Link href="/Courses/1234/Assignments/127" className="wd-assignment-link">
+            A5 - NODE AND SESSION
+          </Link>
           <div className="wd-assignment-meta">
             <span>Multiple Modules</span>
             <span> | <strong> Not available until </strong> June 3 at 12:00am </span>
@@ -68,12 +70,11 @@ export default function Assignments() {
           </div>
         </li>
 
-        {/* A6 */}
         <li className="wd-assignment-list-item">
-          <a href="/Courses/1234/Assignments/128" className="wd-assignment-link">
-           A6 - MONGODB AND MONGOOSE
-          </a>
-           <div className="wd-assignment-meta">
+          <Link href="/Courses/1234/Assignments/128" className="wd-assignment-link">
+            A6 - MONGODB AND MONGOOSE
+          </Link>
+          <div className="wd-assignment-meta">
             <span>Multiple Modules</span>
             <span> | <strong> Not available until </strong> June 10 at 12:00am </span>
             <span> | <strong> Due </strong> June 17 at 11:59pm </span>
@@ -82,4 +83,5 @@ export default function Assignments() {
         </li>
       </ul>
     </div>
-);}
+  );
+}
