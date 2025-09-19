230b755f83c5ade1e16f1612dc0929b9813187fb
kelseywandera <wandera.k@northeastern.edu>
2025-09-19 16:33:03 -0400

    Update Labs landing page with name, section, and links

--- app/Labs/page.tsx
+++ app/Labs/page.tsx
@@ -3,7 +3,7 @@ export default function Labs() {
  return (
    <div id="wd-labs">
      <h1>Labs</h1>
-     <p>Kelsey Wandera – Section 01</p>
+     <p>Kelsey Wandera / Section: CS4550.11597.202610</p>
      <ul>
        <li>
          <Link href="/Labs/Lab1" id="wd-lab1-link">
@@ -17,6 +17,11 @@ export default function Labs() {
          <Link href="/Labs/Lab3" id="wd-lab3-link">
            Lab 3: JavaScript Fundamentals </Link>
        </li>
+        <li>
+          <a href="https://github.com/kelwand/kambaz-next-js" target="_blank">
+            Link to Github: Kambaz Next.js Repository
+          </a>
+        </li>
      </ul>
    </div>
 );}
