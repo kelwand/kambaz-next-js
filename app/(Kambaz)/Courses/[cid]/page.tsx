e993b6813da841dd0e728492efa5b66889990537
kelseywandera <wandera.k@northeastern.edu>
2025-09-19 01:46:47 -0400

    Fix apostrophe escaping in Lab1 and update images

--- app/(Kambaz)/Courses/[cid]/page.tsx
+++ app/(Kambaz)/Courses/[cid]/page.tsx
@@ -1,8 +1,10 @@
 import { redirect } from "next/navigation";
 
+type CoursesPageProps = {
+  params: { cid: string };
+};
 
-export default function CoursesPage({ params }: { params: { cid: string } }) {
- const { cid } = params;
+export default function CoursesPage({ params }: CoursesPageProps) {
+  const { cid } = params;
  redirect("/Courses/${cid}/Home");
 }
-
