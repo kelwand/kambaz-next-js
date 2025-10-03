a3966aad9ed837763827335d6f2ee76427adb5f1
kelseywandera <wandera.k@northeastern.edu>
2025-10-03 10:45:45 -0400

    fix kambaz layout

--- app/(Kambaz)/layout.tsx
+++ app/(Kambaz)/layout.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 import KambazNavigation from "./Navigation";
 import "./styles.css";
 
 export default function AccountLayout({ children }: Readonly<{ children: ReactNode }>) {
   return (
-     <div id="wd-kambaz">
+    <div id="wd-kambaz">
       <div className="d-flex">
-        <KambazNavigation />
-        <div className="wd-main-content-offset p-3 flex-fill" >
+        <Suspense fallback={<div>Loading navigation...</div>}>
+          <KambazNavigation />
+        </Suspense>
+        <div className="wd-main-content-offset p-3 flex-fill">
           {children}
         </div>
       </div>
