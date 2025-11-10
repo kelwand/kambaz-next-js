649fb1265dcbc103ce70735f62bf11e2d71aa61e
kelseywandera <wandera.k@northeastern.edu>
2025-11-10 16:35:06 -0500

    a5 final updates and link to Render

--- app/Labs/TOC.tsx
+++ app/Labs/TOC.tsx
@@ -3,33 +3,40 @@ import { Nav, NavItem, NavLink } from "react-bootstrap";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 export default function TOC() {
-   const pathname = usePathname();
+  const pathname = usePathname();
   return (
-      <Nav variant="pills">
-     <NavItem>
-       <NavLink href="/Labs" as={Link} className={`nav-link ${pathname.endsWith("Labs") ? "active" : ""}`}>
-         Labs </NavLink> </NavItem>
-     <NavItem>
-       <NavLink href="/Labs/Lab1" as={Link} className={`nav-link ${pathname.endsWith("Lab1") ? "active" : ""}`}>
-         Lab 1 </NavLink> </NavItem>
-     <NavItem>
-       <NavLink href="/Labs/Lab2" as={Link} className={`nav-link ${pathname.endsWith("Lab2") ? "active" : ""}`}>
-         Lab 2 </NavLink> </NavItem>
-     <NavItem>
-       <NavLink href="/Labs/Lab3" as={Link} className={`nav-link ${pathname.endsWith("Lab3") ? "active" : ""}`}>
-         Lab 3 </NavLink> </NavItem>
-     <NavItem>
+    <Nav variant="pills">
       <NavItem>
-       <NavLink href="/Labs/Lab4" as={Link} className={`nav-link ${pathname.endsWith("Lab4") ? "active" : ""}`}>
-         Lab 4 </NavLink> </NavItem>
-         <NavItem>
-       <NavLink href="/Labs/Lab5" as={Link} className={`nav-link ${pathname.endsWith("Lab5") ? "active" : ""}`}>
-         Lab 5 </NavLink> </NavItem>
-     <NavItem></NavItem>
-       <NavLink href="/" as={Link}>
-         Kambaz </NavLink> </NavItem>
-     <NavItem>
-       <NavLink href="https://github.com/kelwand">My GitHub</NavLink></NavItem>
-   </Nav>
+        <NavLink href="/Labs" as={Link} className={`nav-link ${pathname.endsWith("Labs") ? "active" : ""}`}>
+          Labs </NavLink> </NavItem>
+      <NavItem>
+        <NavLink href="/Labs/Lab1" as={Link} className={`nav-link ${pathname.endsWith("Lab1") ? "active" : ""}`}>
+          Lab 1 </NavLink> </NavItem>
+      <NavItem>
+        <NavLink href="/Labs/Lab2" as={Link} className={`nav-link ${pathname.endsWith("Lab2") ? "active" : ""}`}>
+          Lab 2 </NavLink> </NavItem>
+      <NavItem>
+        <NavLink href="/Labs/Lab3" as={Link} className={`nav-link ${pathname.endsWith("Lab3") ? "active" : ""}`}>
+          Lab 3 </NavLink> </NavItem>
+      <NavItem>
+        <NavItem>
+          <NavLink href="/Labs/Lab4" as={Link} className={`nav-link ${pathname.endsWith("Lab4") ? "active" : ""}`}>
+            Lab 4 </NavLink> </NavItem>
+        <NavItem>
+          <NavLink href="/Labs/Lab5" as={Link} className={`nav-link ${pathname.endsWith("Lab5") ? "active" : ""}`}>
+            Lab 5 </NavLink> </NavItem>
+        <NavItem></NavItem>
+        <NavLink href="/" as={Link}>
+          Kambaz </NavLink> </NavItem>
+      <NavItem>
+        <NavLink href="https://github.com/kelwand">
+          My GitHub</NavLink></NavItem>
+      <NavItem>
+        <NavLink href="https://github.com/kelwand/kambaz-node-server-app">
+          Node Server GitHub Repo </NavLink></NavItem>
+      <NavItem>
+        <NavLink href="https://kambaz-node-server-app-enzr.onrender.com">
+          Live Node Server on Render </NavLink></NavItem>
+    </Nav>
   );
 }
