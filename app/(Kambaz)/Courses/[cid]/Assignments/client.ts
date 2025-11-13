d8e928f096557cde40324b7fd8ddf5d5b6a676c1
kelseywandera <wandera.k@northeastern.edu>
2025-11-13 17:20:45 -0500

    Fix assignment client routes

--- app/(Kambaz)/Courses/[cid]/Assignments/client.ts
+++ app/(Kambaz)/Courses/[cid]/Assignments/client.ts
@@ -1,24 +1,41 @@
 import axios from "axios";
 
-const SERVER = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:4000";
-const ASSIGNMENTS_API = `${SERVER}/api`;
+const SERVER =
+  process.env.NEXT_PUBLIC_HTTP_SERVER || "http://localhost:4000";
 
-export const findAssignmentsForCourse = async (moduleId: string) => {
-  const response = await axios.get(`${ASSIGNMENTS_API}/modules/${moduleId}/assignments`);
+const axiosWithCredentials = axios.create({
+  withCredentials: true,
+});
+
+export const findAssignmentsForCourse = async (courseId: string) => {
+  const response = await axios.get(
+    `${SERVER}/api/courses/${courseId}/assignments`
+  );
   return response.data;
 };
 
-export const createAssignment = async (moduleId: string, assignment: any) => {
-  const response = await axios.post(`${ASSIGNMENTS_API}/modules/${moduleId}/assignments`, assignment);
+export const createAssignment = async (courseId: string, assignment: any) => {
+  const response = await axiosWithCredentials.post(
+    `${SERVER}/api/courses/${courseId}/assignments`,
+    assignment
+  );
   return response.data;
 };
 
-export const updateAssignment = async (assignmentId: string, updates: any) => {
-  const response = await axios.put(`${ASSIGNMENTS_API}/assignments/${assignmentId}`, updates);
+export const updateAssignment = async (
+  assignmentId: string,
+  updates: any
+) => {
+  const response = await axiosWithCredentials.put(
+    `${SERVER}/api/assignments/${assignmentId}`,
+    updates
+  );
   return response.data;
 };
 
 export const deleteAssignment = async (assignmentId: string) => {
-  const response = await axios.delete(`${ASSIGNMENTS_API}/assignments/${assignmentId}`);
+  const response = await axiosWithCredentials.delete(
+    `${SERVER}/api/assignments/${assignmentId}`
+  );
   return response.data;
 };
